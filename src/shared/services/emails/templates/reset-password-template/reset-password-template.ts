453e10cf91e3378501d3dd02e915a86af6625403
lovin1015 <lovinhood1015@gmail.com>
2023-07-19 14:14:03 +0630

    refactor(): move file inside folder

---
+++ src/shared/services/emails/templates/reset-password-template/reset-password-template.ts
@@ -0,0 +1,25 @@
+import { IResetPasswordParams } from '@user/interfaces/user.interface';
+import ejs from 'ejs';
+import fs from 'fs';
+
+class ResetPasswordTemplate {
+  public passwordResetConfirmationTemplate(
+    templateParams: IResetPasswordParams
+  ): string {
+    const { username, email, ipaddress, date } = templateParams;
+    return ejs.render(
+      fs.readFileSync(__dirname + '/reset-password-template.ejs', 'utf8'),
+      {
+        username,
+        email,
+        ipaddress,
+        date,
+        image_url:
+          'https://w7.pngwing.com/pngs/120/102/png-transparent-padlock-logo-computer-icons-padlock-technic-logo-password-lock.png',
+      }
+    );
+  }
+}
+
+export const resetPasswordTemplate: ResetPasswordTemplate =
+  new ResetPasswordTemplate();
